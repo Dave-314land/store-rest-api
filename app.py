76d4e6ea1857ac24f003d804bcef8083e9af50cd
David Piland <dpil8204@TELETRACKING.COM>
2022-08-05 15:24:11 -0400

    converting item endpoints into RESTful resources through the use of the Flask-Restful module

--- app.py
+++ app.py
@@ -1,7 +1,35 @@
 from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
+items = []
+
+class Items(Resource):
+    def get(self):
+        return {'items': items}, 200
+
+class Item(Resource):
+    def get(self, name):
+        for item in items:
+            if item['name'] == name:
+              return item
+        return {'item': None}, 404
+    
+    def post(self, name):
+        data = request.get_json()
+        item = {
+            'name': name,
+            'price': data['price']
+        }
+        items.append(item)
+        return item, 201
+
+api.add_resource(Items, '/items') # http://127.0.0.1:5000/items
+api.add_resource(Item, '/item/<string:name>') # http://127.0.0.1:5000/item/apple
+
+"""
 stores = [
     {
         'name': 'Superpowers store',
@@ -17,7 +45,6 @@ stores = [
         ]
     }
 ]
-
 
 # POST /store data: {name:}
 @app.route('/store', methods=['POST'])
@@ -65,6 +92,7 @@ def get_items_in_store(name):
         if store['name'] == name:
             return jsonify({'items': store['items']})
     return jsonify({'message': 'store not found'})
+"""
 
-
-app.run(port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
