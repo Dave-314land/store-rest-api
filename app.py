a4f0e608deb309ad7b24297f244410575caae2a7
David Piland <dpil8204@TELETRACKING.COM>
2022-08-10 16:28:02 -0400

    refactored item endpoints into separate module

--- app.py
+++ app.py
@@ -1,69 +1,19 @@
-from flask import Flask, jsonify, request
-from flask_restful import Resource, Api, reqparse
-from flask_jwt import JWT, jwt_required
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+from decouple import config
+
 from security import authenticate, identity
 from user import UserRegister
+from item import Items, Item
 
-from decouple import config
+
 API_JWT_SECRET_KEY = config('JWT_SECRET_KEY')
-
 app = Flask(__name__)
 app.secret_key = API_JWT_SECRET_KEY
 api = Api(app)
 
 jwt = JWT(app, authenticate, identity)
-
-items = []
-
-class Items(Resource):
-    def get(self):
-        return {'items': items}, 200
-
-class Item(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument(
-        'price',
-        type=float,
-        required=True,
-        help="This field cannot be left blank!"
-    )
-
-    @jwt_required()
-    def get(self, name):
-        for item in items:
-            if item['name'] == name:
-              return {'item': item}, 200
-        return {'item': None}, 404
-    
-    def post(self, name):
-        for item in items:
-            if item['name'] == name:
-                return {'message': f'An item with {name} already exists.'}, 400
-        data = Item.parser.parse_args()
-        item = {
-            'name': name,
-            'price': data['price']
-        }
-        items.append(item)
-        return item, 201
-
-    def put(self, name):
-        data = Item.parser.parse_args()
-        for item in items:
-            if item['name'] != name:
-                item = {
-                    'name': name,
-                    'price': data['price']
-                }
-                items.append(item)
-            else:
-                item.update(data)
-            return item
-    
-    def delete(self, name):
-        global items
-        items = list(filter(lambda x:x['name'] != name, items))
-        return {'message': 'item deleted'}
 
 api.add_resource(Items, '/items') # http://127.0.0.1:5000/items
 api.add_resource(Item, '/item/<string:name>') # http://127.0.0.1:5000/item/apple
--- create_tables.py
+++ create_tables.py
@@ -3,8 +3,14 @@ import sqlite3
 con = sqlite3.connect('data.db')
 cur = con.cursor()
 
-create_table = "CREATE TABLE if not exists users (id INTEGER PRIMARY KEY, username text, password text)"
-cur.execute(create_table)
+create_table_users = "CREATE TABLE if not exists users (id INTEGER PRIMARY KEY, username text, password text)"
+create_table_items = "CREATE TABLE if not exists items (item_name text, price real)"
+
+cur.execute(create_table_users)
+cur.execute(create_table_items)
+
+# Test data - delete later
+cur.execute("INSERT INTO items VALUES ('test_item', 10.99)")
 
 con.commit()
 con.close()
