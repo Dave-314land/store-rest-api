5c90f96cb89a881637ecfdd067473e631ba67e6e
David Piland <dpil8204@TELETRACKING.COM>
2022-09-14 11:52:31 -0400

    added store model and relationship to item model

--- app.py
+++ app.py
@@ -21,77 +21,12 @@ api = Api(app)
 def create_tables():
     db.create_all()
 
-
 jwt = JWT(app, authenticate, identity) #/auth
 
 api.add_resource(Items, '/items') # http://127.0.0.1:5000/items
 api.add_resource(Item, '/item/<string:name>') # http://127.0.0.1:5000/item/apple
 api.add_resource(UserRegister, '/register') # http://127.0.0.1:5000/register
 
-"""
-stores = [
-    {
-        'name': 'Superpowers store',
-        'items': [
-            {
-                'name': 'Flight',
-                'price': 7000.00
-            },
-            {
-                'name': 'Super Strength',
-                'price': 8000.00
-            }
-        ]
-    }
-]
-
-# POST /store data: {name:}
-@app.route('/store', methods=['POST'])
-def create_store():
-    # Create a new store with an empty list of items
-    request_data = request.get_json()
-    new_store = {
-        'name': request_data['name'],
-        'items': []
-    }
-    stores.append(new_store)
-    return jsonify(new_store)
-
-# GET /store/<string:name>
-@app.route('/store/<string:name>')
-def get_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify(store)
-    return jsonify({'message': 'store not found'})
-
-# GET /store
-@app.route('/store')
-def get_stores():
-    return jsonify({'stores': stores})
-
-# POST /store/<string:name>/item {name:, price:}
-@app.route('/store/<string:name>/item', methods=['POST'])
-def create_item_in_store(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store['name'] == name:
-            new_item = {
-                'name': request_data['name'],
-                'price': request_data['price']
-            }
-            store['items'].append(new_item)
-            return jsonify(store)
-    return jsonify({'message': 'store not found'})
-
-# GET /store/<string:name>/item
-@app.route('/store/<string:name>/item')
-def get_items_in_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify({'items': store['items']})
-    return jsonify({'message': 'store not found'})
-"""
 
 if __name__ == '__main__':
     db.init_app(app)
